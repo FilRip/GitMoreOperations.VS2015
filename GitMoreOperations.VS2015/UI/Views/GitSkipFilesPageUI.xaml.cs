1dc04f555f078f63b2e2769b523bae7be122a99a
FilRip <filrip@yahoo.fr>
2020-02-04 22:15:28 +0100

    Fix rafraichir fenetre git skip worktree

--- GitMoreOperations.VS2015/UI/Views/GitSkipFilesPageUI.xaml.cs
+++ GitMoreOperations.VS2015/UI/Views/GitSkipFilesPageUI.xaml.cs
@@ -29,20 +29,13 @@ namespace GitMoreOperations.VS2015.UI.Views
             model.Update();
         }
 
-        void TestUserControl_ContentRendered(object sender, EventArgs e)
-        {
-            // Don't forget to unsubscribe from the event
-            ((PresentationSource)sender).ContentRendered -= TestUserControl_ContentRendered;
-
-            model.Update();
-        }
-
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            PresentationSource presentationSource = PresentationSource.FromVisual((Visual)sender);
-
-            // Subscribe to PresentationSource's ContentRendered event
-            presentationSource.ContentRendered += TestUserControl_ContentRendered;
+            if (_firstTime)
+            {
+                _firstTime = false;
+                model.Update();
+            }
         }
     }
 }
